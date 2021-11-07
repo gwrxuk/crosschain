4b846e5c39410555a2977fd308829faae8aa61eb
Emile Liu <emile@ampifire.com>
2021-11-07 22:54:17 +0800

    try

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # crosschain
+Refer to https://rocket.rs/v0.5-rc/guide/getting-started/
--- router/src/main.rs
+++ router/src/main.rs
@@ -4,6 +4,10 @@ fn index() -> &'static str{
 	"Cross Chain"
 }
 
+#[get("/world")]
+fn world()-> &'static str{
+	"hello world"
+}
 #[launch]
 fn rocket() -> _{
 	rocket::build().mount("/",routes![index])
